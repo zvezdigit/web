28fd8ef863f6b972b0b81fd1b3bbcb84dd795f46
zvezdigit <zvezdelina.vasileva@gmail.com>
2022-02-07 17:01:14 +0200

    UserService added+

--- BasicWebServer/BasicWebServer/Services/UserService.cs
+++ BasicWebServer/BasicWebServer/Services/UserService.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace BasicWebServer.Demo.Services
 {
-    internal class UserService
+    public class UserService
     {
+        private const string Username = "user";
+
+        private const string Password = "user123";
+
+        public bool IsLoginCorrect(string username, string password)
+            => username == Username && password == Password;
     }
 }
--- BasicWebServer/BasicWebServer/StartUp.cs
+++ BasicWebServer/BasicWebServer/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using BasicWebServer.Demo.Controllers;
+using BasicWebServer.Demo.Services;
 using BasicWebServer.Serverr;
 using BasicWebServer.Serverr.HTTP;
 using BasicWebServer.Serverr.Routing;
@@ -9,21 +10,13 @@ public class StartUp
     public static async Task Main()
     {
 
-        await new HttpServer(routes => routes
-        //.MapGet<HomeController>("/", c => c.Index())
-        //.MapGet<HomeController>("/Redirect", c => c.Redirect())
-        //.MapGet<HomeController>("/HTML", c => c.Html())
-        //.MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
-        //.MapGet<HomeController>("/Content", c => c.Content())
-        //.MapPost<HomeController>("/Content", c => c.DownloadContent())
-        //.MapGet<HomeController>("/Cookies", c => c.Cookies())
-        //.MapGet<HomeController>("/Session", c => c.Session())
-        //.MapGet<UserController>("/LogIn", c => c.Login())
-        //.MapPost<UserController>("/LogIn", c => c.LogInUser())
-        //.MapGet<UserController>("/Logout", c => c.LogOut())
-        //.MapGet<UserController>("/UserProfile", c => c.GetUserData()))
-         .MapControllers())
-        .Start();
+        var server = new HttpServer(routes => routes
+             .MapControllers());
+
+        server.ServiceCollection
+            .Add<UserService>();
+
+        await server.Start();
 
 
     }
